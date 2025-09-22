f1c14733133c1bf156ee06ff9306d1e9836ceef2
johnkauan <johnkauanpassos123@gmail.com>
2025-09-22 14:34:27 -0300

    atualizando insertion_sort.h

--- src/insertion_sort.h
+++ src/insertion_sort.h
@@ -3,7 +3,10 @@
 
 
 void insertion(int vetor[], int n);
-
+void gerar_crescente(int vetor[], int n);
+void gerar_decrescente(int vetor[], int n);
+void gerar_randomico(int vetor[], int n);
+void salvar_dados(const char* nome_dados, int vetor[], int n);
 void insertion_menu();
 
 #endif
